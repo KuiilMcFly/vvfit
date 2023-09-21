f82b4bf4ca0030563d8b4e5b06bda9aed95b96b6
KuiilMcFly <carmine.esp40@gmail.com>
2023-09-21 22:50:07 +0200

    VVfit Project start

---
+++ app/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+import "../Styles/homepage.scss";
+
+const HomePage = () => {
+  return (
+    <header>
+      <h1 className="header-title">VVfit</h1>
+    </header>
+  );
+};
+
+export default HomePage;
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import HomePage from "./Home/HomePage";
+import "./Styles/app.scss";
+
+export default function Home() {
+  return (
+    <main>
+      <HomePage />
+    </main>
+  );
+}
